3765a4a4d4ca88bdedf44208b4a086e592b7e111
isamaues <42849591+isamaues@users.noreply.github.com>
2020-07-29 00:58:44 -0300

    Delete serialtst.py
    this uses the bluetooth pin, now using rfcomm.

--- serialtst.py
+++
@@ -1,7 +0,0 @@
-import serial
-ser = serial.Serial("/dev/ttyAMA0") #bluetooth pin raspberry pi 3 b
-print(ser.name)
-print(ser.is_open) #True
-ser.write(b"hello")
-ser.close()
-print(ser.is_open) #False
