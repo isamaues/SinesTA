852c642c2183e169574fca6a09d0a0c44b877614
isamaues <42849591+isamaues@users.noreply.github.com>
2020-07-29 01:01:59 -0300

    Add files via upload
    First working test with ESP32. Reads the serial message.

---
+++ BT_Test3.ino
@@ -0,0 +1,38 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+int LED_BUILTIN = 2;
+void setup() {
+  Serial.begin(9600);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  //SerialBT.enableSSP();
+  //SerialBT.connect("B8:27:EB:8A:3D:B2");
+  //SerialBT.connect("raspberrypi");
+  SerialBT.available();
+  Serial.println("Connected, i guess");
+  pinMode (LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
