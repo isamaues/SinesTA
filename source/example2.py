2fff13c01904a25cc9cc9f7856b7e1f02d9bfb46
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-05 20:25:36 -0300

    Update and rename example2.py to source/example2.py

--- source/example2.py
+++ source/example2.py
@@ -2,4 +2,4 @@
 import os
 for i in range(5):
   os.system("arecord -D hw:1,0 -f S16_LE -c1 -r44100 -s 43000 test.wav")
-  os.system("python3 inference8.py test.wav")
+  os.system("python3 inference9.py test.wav")
