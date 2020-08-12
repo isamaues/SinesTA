7390c8a1a93487f1f2cf0afca7a0e2b8ed910686
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-12 00:33:41 -0300

    Update installer.sh
    remmoved some comments

--- source/installer.sh
+++ source/installer.sh
@@ -3,8 +3,6 @@ echo "starting installation --------------------"
 cd
 apt-get update
 pip3 install --upgrade setuptools
-#echo "cloning repository"
-#git clone https://github.com/isamaues/tflite_model_audioset_yamnet_modified
 echo "installing prerequisite packages"
 pip3 install numpy
 echo "numpy ok"
@@ -21,18 +19,8 @@ sudo apt-get install cmake -y
 sudo apt-get install libedit-dev -y
 #apt-get install clang-9 lldb-9 lld-9 -y
 sudo apt-get install libllvm-9-ocaml-dev libllvm9 llvm-9 llvm-9-dev llvm-9-doc llvm-9-examples llvm-9-runtime -y
-#sudo apt-get install libllvm-9-ocaml-dev libllvm9 llvm-9 llvm-9-dev llvm-9-doc llvm-9-runtime -y
-#which llvm-config
-#sudo find / -iname llvm_config
-#LLVM_CONFIG=<path>
-#LLVM_CONFIG=/opt/llvm/bin/llvm-config sudo pip3 install llvmlite
 LLVM_CONFIG=/usr/lib/llvm-9/bin/llvm-config pip3 install llvmlite
-#get into llvmlite directory
-#cd llvmlite
-#cd /home/pi/.local/lib/python3.7/site-packages/llvmlite
 cd /usr/local/lib/python3.7/dist-packages/llvmlite
-#python3 setup.py build
-#python3 setup.py install
 echo "llvmlite ok"
 cd
 pip3 install --upgrade colorama
