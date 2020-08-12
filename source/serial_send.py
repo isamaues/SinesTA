7e9d9a06a645f757f606b27c7cbed58029b19267
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-12 00:08:37 -0300

    Update serial_send.py

--- source/serial_send.py
+++ source/serial_send.py
@@ -1,9 +1,9 @@
 import serial
-import time
+#import time
 import sys
 def main(argv):
     ser = serial.Serial('/dev/rfcomm0', 9600)
-    time.sleep(3)
+    #time.sleep(3)
     temp = '[' + argv[0] + ']'
     ser.write(temp.encode('utf-8'))
     ser.close()
