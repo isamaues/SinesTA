13ead59a6e1fa80f74295340e6f469ed6d432527
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-12 00:30:13 -0300

    Update SinesTA.sh

--- source/SinesTA.sh
+++ source/SinesTA.sh
@@ -8,7 +8,7 @@ echo "$RFCOMM OK"
 
 while True
 do
-  os.system("arecord -D hw:1,0 -f S16_LE -c1 -r44100 -s 43000 test.wav")
-  os.system("python3 inference9.py test.wav")
+  os.system("arecord -D hw:1,0 -f S16_LE -c1 -r44100 -s 43000 temp.wav")
+  os.system("python3 inference_bt.py temp.wav")
   #sleep 1
 done
