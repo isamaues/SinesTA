183e13b32e39c7e734e0cb5636c10d1e117f0005
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-11 23:44:56 -0300

    Update set_rfcomm.sh
    -f is a regular file, -S is a socket file

--- source/set_rfcomm.sh
+++ source/set_rfcomm.sh
@@ -3,7 +3,7 @@
 #check if rfcomm0 file exists
 #https://linuxize.com/post/bash-check-if-file-exists/#:~:text=Check%20if%20File%20Exists,-When%20checking%20if&text=You%20can%20also%20use%20the,echo%20%22%24FILE%20exists.%22
 FILE=/dev/rfcomm0
-if [ ! -f "$FILE" ]; then
+if [ ! -S "$FILE" ]; then
     echo "$FILE does not exist. Creating socket"
     #if it does not exists: rfcomm manual bind
     sudo rfcomm bind rfcomm0 ESP32_SinesTA
