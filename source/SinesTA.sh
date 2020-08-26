a66d65558849182bbb5a8688b53999696b15e65e
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-25 23:23:12 -0300

    Update SinesTA.sh

--- source/SinesTA.sh
+++ source/SinesTA.sh
@@ -8,7 +8,11 @@ echo "$RFCOMM OK"
 
 while True
 do
+  echo "Press <CTRL+C> to exit."
   os.system("arecord -D hw:1,0 -f S16_LE -c1 -r44100 -s 43000 temp.wav")
   os.system("python3 inference_bt.py temp.wav")
   #sleep 1
+  #if [[ "$i" == '2' ]]; then # IF KEYBOARD INTERRUPT?
+    #break
+  #fi
 done
