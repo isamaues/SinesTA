e342565baf2a97ef4b071bd1823bc5f86a4d920e
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-27 17:53:52 -0300

    Update set_rfcomm.sh

--- source/set_rfcomm.sh
+++ source/set_rfcomm.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "Checking RFCOMM."
+#check if rfcomm0 file exists
+FILE=/dev/rfcomm0
+if [ ! -S "$FILE" ]; then
+    echo "$FILE does not exist. Creating socket"
+    #if it does not exists: rfcomm manual bind
+    echo "Binding Devices."
+    sudo rfcomm bind rfcomm0 XX:XX:XX:XX:XX:XX
+fi
+echo "RFCOMM OK"
