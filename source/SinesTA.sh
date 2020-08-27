9c35fefe05a239c83086d91e6370e71fad0c7af9
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-27 14:02:34 -0300

    Update SinesTA.sh
    removing python like code

--- source/SinesTA.sh
+++ source/SinesTA.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-import os
 
 echo "Checking RFCOMM."
 sh ./set_rfcomm.sh
 #except
 echo "$RFCOMM OK"
 
-while True
+while :
 do
-  echo "Press <CTRL+C> to exit."
-  os.system("arecord -D hw:1,0 -f S16_LE -c1 -r44100 -s 43000 temp.wav")
-  os.system("python3 inference_bt.py temp.wav")
-  #sleep 1
-  #if [[ "$i" == '2' ]]; then # IF KEYBOARD INTERRUPT?
-    #break
-  #fi
+  echo "Press CTRL+Z to EXIT."
+  arecord -D hw:1,0 -f S16_LE -c1 -r44100 -s 43000 temp.wav
+	python3 inference_bt.py temp.wav
 done
+
