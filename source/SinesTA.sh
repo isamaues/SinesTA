6592ee63f4ef5476e5a299fd3a67e347b16046f8
isamaues <42849591+isamaues@users.noreply.github.com>
2020-08-27 18:06:52 -0300

    Update SinesTA.sh

--- source/SinesTA.sh
+++ source/SinesTA.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-echo "Checking RFCOMM."
-#check if rfcomm0 file exists
-FILE=/dev/rfcomm0
-if [ ! -S "$FILE" ]; then
-    echo "$FILE does not exist. Creating socket"
-    #if it does not exists: rfcomm manual bind
-    echo "Binding Devices."
-    sudo rfcomm bind rfcomm0 ESP32_SinesTA
-fi
-echo "RFCOMM OK"
+sudo bash set_rfcomm.sh
 
 while :
 do
